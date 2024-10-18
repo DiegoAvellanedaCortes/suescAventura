ce0f9690a6da69aa42ae997bb765828964854bca
Diego Avellaneda <daacdiego2015@gmail.com>
2024-10-18 11:00:16 -0500

    El sistema de rutas funciona correctamente, el diseño del inicio fondo, mensaje de acción y boton funcionan correctamente, agregamos el boton de whatsapp flotante y ya es funcional

--- src/Inicio/index.js
+++ src/Inicio/index.js
@@ -1,9 +1,24 @@
+import "./inicio.css";
+import fondo from "../img/FondoInicio.png";
+
 function Inicio() {
     return (
         <div className="home">
-            <h2>Inicio</h2>
+            <div className="imagen-fondo-inicio">
+                <img src={fondo} alt="fondo-inicio" />
+            </div>
+            <div className="boton-inicio">
+                <p>¿Estás preparado</p>
+                <p>para la <strong>aventura?</strong></p>
+                <button className="boton-contacto">
+                    <a target="_blank" rel="noreferrer" href="https://wa.me/573204543746">Contáctanos</a>
+                </button>
+            </div>
+            <div className="contenido-inicio">
+
+            </div>
         </div>
     );
 }
 
-export {Inicio}
+export { Inicio }
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,12 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
-import "./Layout.css"
+import "./Layout.css";
 import logo from "../img/SuescAventura.png";
+import logo_whatsapp from "../img/whatsapp.png";
 
 function Layout() {
     return (
         <div className="layout">
             <div className="menu">
-                <img src={logo} className="img-navbar" />
+                <img src={logo} className="img-navbar" alt="Logo SuescAventura" />
                 <nav>
                     <ul>
                         <li>
@@ -23,6 +24,11 @@ function Layout() {
                         </li>
                     </ul>
                 </nav>
+            </div>
+            <div className="boton-whatsapp">
+                <a target="_blank" rel="noreferrer" href="https://wa.me/573204543746">
+                    <img src={logo_whatsapp} alt="logo WhatsApp" />
+                </a>
             </div>
             <Outlet />
             <footer>
