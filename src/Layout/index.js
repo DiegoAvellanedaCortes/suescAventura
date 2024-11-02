bd7c08e486a7fd145edd9233711eea4d57bb63f1
Diego Avellaneda <daacdiego2015@gmail.com>
2024-11-02 16:38:23 -0500

    Agregamos footer con diseño y funcionalidad

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,8 +2,20 @@ import { Outlet, Link } from "react-router-dom";
 import "./Layout.css";
 import logo from "../img/SuescAventura.png";
 import logo_whatsapp from "../img/whatsapp.png";
+import icoYoutube from "../img/Youtube.png";
+import icoInstagram from "../img/Instagram.png";
+import icoFacebook from "../img/Facebook.png";
+import icoTiktok from "../img/Tiktok.png";
 
 function Layout() {
+
+    const redes = [
+        { name_red: "Youtube", logo: icoYoutube, url: "https://www.instagram.com/suescaventura" },
+        { name_red: "Instagram", logo: icoInstagram, url: "https://www.instagram.com/suescaventura" },
+        { name_red: "Facebook", logo: icoFacebook, url: "https://www.facebook.com/profile.php?id=100063543973730" },
+        { name_red: "Tiktok", logo: icoTiktok, url: "https://www.tiktok.com/@suescaventura" },
+    ]
+
     return (
         <div className="layout">
             <div className="menu">
@@ -32,7 +44,39 @@ function Layout() {
             </div>
             <Outlet />
             <footer>
-                <p>Este es el footer</p>
+                <div className="logo-footer">
+                    <img src={logo} className="img-navbar" alt="Logo SuescAventura" />
+                </div>
+                <div className="contenido-footer">
+                    <div className="redes">
+                        <ul>
+                            {redes.map(item => (
+                                <li key={item.name_red}>
+                                    <a href={item.url} target="_blank" rel="noreferrer">
+                                        <img src={item.logo} alt={item.name_red} />
+                                        {item.name_red}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="links">
+                        <ul>
+                            <li>
+                                <Link to="/">Inicio</Link>
+                            </li>
+                            <li>
+                                <Link to="/planes">Planes</Link>
+                            </li>
+                            <li>
+                                <Link to="/contacto">Contacto</Link>
+                            </li>
+                            <li>
+                                <Link to="/quienes-somos">Quienes Somos?</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </footer>
         </div>
     );
--- src/bannerClientes/index.js
+++ src/bannerClientes/index.js
@@ -2,23 +2,30 @@ import "./bannerClientes.css";
 import Caminata from "../img/Caminata.jpg";
 import Escalada from "../img/Escalada.jpg";
 import Cueva from "../img/Cueva.jpeg";
+import Cuarta from "../img/Cuarta.png";
+
+
 
 function BannerClientes() {
+
+    const fotos_clientes = [
+        {url: Caminata, alt: "Caminata", hidden:true },
+        {url: Escalada,alt: "Escalada", hidden:true},
+        {url: Cueva,alt: "Cueva", hidden:true},
+        {url: Cuarta,alt: "Cuarta", hidden:false},
+    ];
+
     return (
         <section className="banner-clientes">
             <div className="titulo-clientes">
                 <p>Nuestros Clientes</p>
             </div>
             <div className="banner">
-                <div className="card-imagenes">
-                    <img src={Caminata} alt="Caminata" />
-                </div>
-                <div className="card-imagenes">
-                    <img src={Escalada} alt="Caminata" />
-                </div>
-                <div className="card-imagenes">
-                    <img src={Cueva} alt="Caminata" />
-                </div>
+                {fotos_clientes.map(item => (  
+                    <div className={`card-imagenes ${item.hidden? "active":"inactive"}`} key={item.alt}>
+                        <img src={item.url} alt={item.alt}/>
+                    </div>
+                ))}
             </div>
         </section>
     );
