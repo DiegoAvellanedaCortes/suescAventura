5b68446e97a99480c12fb6bdd1f0d14c756d1174
Diego Avellaneda <daacdiego2015@gmail.com>
2025-02-01 09:27:54 -0500

    Finalizamos diseño

--- src/Quienes/index.js
+++ src/Quienes/index.js
@@ -6,7 +6,7 @@ function Quienes() {
         <section className="quienes_somos">
             <img className="foto_equipo" src={Equipo} alt="equipo" />
             <div className="registro_turismo">
-                <p className="titulo_registro_turismo">Registro Nacional de Turismo (RTN) <br />
+                <p className="titulo_registro_turismo">Registro Nacional de Turismo (RTN) 
                     SuescAventura - 65563</p>
                 <p className="descripcion_agencia">
                     Somos operadores turísticos en el municipio de Suesca (Cundinamarca) desde el 2015, legalmente
@@ -16,15 +16,13 @@ function Quienes() {
                 </p>
             </div>
             <section className="valores">
-                <p>Nuestros Valores:</p>
-                <p>
-                    <ul>
-                        <li>Calidad: Ofrecemos solo lo mejor, cuidando cada detalle para que tu aventura sea perfecta.</li>
-                        <li>Pasión: Amamos lo que hacemos, y esa pasión se refleja en cada uno de nuestros servicios.</li>
-                        <li>Compromiso: Nos comprometemos a brindar un servicio excepcional, siempre poniendo al cliente en el centro de todo.</li>
-                        <li>Innovación: Nos mantenemos a la vanguardia, buscando siempre nuevas formas de sorprender y deleitar a nuestros clientes.</li>
-                    </ul>
-                </p>
+                <p className="titulo_valores">Nuestros Valores:</p>
+                <ul className="lista_valores">
+                    <li>Calidad: Ofrecemos solo lo mejor, cuidando cada detalle para que tu aventura sea perfecta.</li>
+                    <li>Pasión: Amamos lo que hacemos, y esa pasión se refleja en cada uno de nuestros servicios.</li>
+                    <li>Compromiso: Nos comprometemos a brindar un servicio excepcional, siempre poniendo al cliente en el centro de todo.</li>
+                    <li>Innovación: Nos mantenemos a la vanguardia, buscando siempre nuevas formas de sorprender y deleitar a nuestros clientes.</li>
+                </ul>
             </section>
         </section>
     );
